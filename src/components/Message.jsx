c19f10daba081af4bf5b3b75b62de982ee96717c
Ben Tucker <bwtucker4@gmail.com>
2022-09-12 09:53:08 -0700

    Implement useRef to scroll page when a message is sent

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,19 +1,26 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import { useEffect } from 'react';
 import Moment from "react-moment";
 import "../styles/Message.css";
 
 const Message = ({ msg, currentUser }) => {
-  return (
-    <div className={`message-wrapper ${msg.from === currentUser ? "sender" : "recipient"}`}>
-        <div className='message-contents'>
-            <p>{msg.image ? <img src={msg.image} alt={msg.message}></img> : null}</p>
-            <p>{msg.message}</p>
-            <small>
-                <Moment fromNow>{msg.createdAt.toDate()}</Moment>
-            </small>
+    const scrollRef = useRef()
+
+    useEffect(() => {
+        scrollRef.current?.scrollIntoView()
+    }, [msg]);
+
+    return (
+        <div className={`message-wrapper ${msg.from === currentUser ? "sender" : "recipient"}`} ref={scrollRef}>
+            <div className='message-contents'>
+                <p>{msg.image ? <img src={msg.image} alt={msg.message}></img> : null}</p>
+                <p>{msg.message}</p>
+                <small>
+                    <Moment fromNow>{msg.createdAt.toDate()}</Moment>
+                </small>
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Message
